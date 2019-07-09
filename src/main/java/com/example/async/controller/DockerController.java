cf10551c493fc17f6a3164d6aa55078ebae9730d
彬 <17661977890@163.com>
2019-07-09 11:52:57 +0800

    ss

---
+++ src/main/java/com/example/async/controller/DockerController.java
@@ -0,0 +1,17 @@
+package com.example.async.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author 彬
+ * @Date 2019/7/9
+ */
+@RestController
+public class DockerController {
+
+    @RequestMapping
+    public String testDockerDeploy(){
+        return "hello,this is my first docker deploy in idea!";
+    }
+}
